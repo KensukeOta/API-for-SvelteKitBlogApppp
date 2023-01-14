9c0edf615f92b762a2df90e98452caea1e00265a
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-01-14 13:17:02 +0900

    CORSの設定を実装

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from routers import users
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "https://sveltekit-blogapp-kensuke.vercel.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(users.router)
 
 
 @app.get("/")
